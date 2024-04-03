e153ba29a9e8c85e3c896af3b8d08535711e53f4
B. Ricks <bricks@unomaha.edu>
2024-04-03 11:47:35 -0500

    Setup for class

--- games/communication-game/components/ControllerComponent.js
+++ games/communication-game/components/ControllerComponent.js
@@ -3,26 +3,25 @@ class ControllerComponent extends Component {
     //Get both game objects,
     //grab their geometry and check 
     //if there is a collision
-    let playerCircle = GameObject.find("PlayerGameObject").getComponent("Circle").asGeometry();
-    let deathCircle = GameObject.find("DeathGameObject").getComponent("Circle").asGeometry();
-    let inCollision =  CollisionsGeometric.isCircle2Circle2Collision(playerCircle, deathCircle);
 
-    if(Globals.communicationMethod == "sameScene")
+
+    if (Globals.communicationMethod == "sameScene") {
       //If we are communicating between objects in the same scene
       //find the game object in question and update its inCollision 
       //property
-      GameObject.find("PlayerGameObject").getComponent("PlayerComponent").inCollision = inCollision;
-    
-    if(Globals.communicationMethod == "events")
+    }
+
+    if (Globals.communicationMethod == "events") {
       //If we are communicating using events,
       //fire an event
-      EventSystem.fireEvent({inCollision:inCollision})
-    
-    if(Globals.communicationMethod == "globals")
+    }
+
+    if (Globals.communicationMethod == "globals") {
       //If we are communicating using globals, 
       //update the global variable
       Globals.inCollision = inCollision
-    
+    }
+
   }
 }
 
--- games/communication-game/components/PlayerComponent.js
+++ games/communication-game/components/PlayerComponent.js
@@ -1,22 +1,17 @@
 class PlayerComponent extends Component {
     start(ctx) {
         //If we are using the event system, we have to listen for events
-        if (Globals.communicationMethod == "events") {
-            EventSystem.registerListener(this);
-        }
+        
     }
     handleEvent(event) {
         //Respond to an event
         //Note that we assume that there is only one kind of event
         //that can be fired and that it has a property called inCollision
-        this.inCollision = event.inCollision
     }
     update(ctx) {
         //If we are communicating using the Globals global class
         //then update our value based on that
-        if(Globals.communicationMethod == "globals")
-            this.inCollision = Globals.inCollision;
-        
+       
         if (this.inCollision) 
             //Example of same game object communication
             this.parent.getComponent("Circle").fill = "red"
